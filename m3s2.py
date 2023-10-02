0ea7d3200f86ebbe131edda54512f41dd94719b3
Renan Okanha <okanharenan@gmail.com>
2023-10-02 20:40:42 -0300

    Exercicio da ultima m3s2

---
+++ m3s2.py
@@ -0,0 +1,34 @@
+def decorator_imprimir(func):
+   
+    def imprimindo(capital,taxa,tempo):
+        """
+        Função interna do decorator que imprime os valores dos parâmetros e o resultado da função.
+        
+        Args:
+            capital (float): O capital principal.
+            taxa (float): A taxa de juros.
+            tempo (float): O tempo (em anos).
+        """
+        print(f'CAPITAL: {capital} TAXA: {taxa} TEMPO:{tempo}')
+
+        resultado = func(capital,taxa,tempo) # vai me trazer o resultado da função
+
+        print(f'Resultado:{resultado}') #imprime o resultado da soma 
+        
+    return imprimindo
+
+@decorator_imprimir
+def juros_simples(capital,taxa,tempo):
+    '''
+    função para calcular juros simples 
+
+    Args:
+        capital (float): O capital principal.
+        taxa (float): A taxa de juros.
+        tempo (float): O tempo (em anos).
+
+    '''
+    return capital* (taxa/100)* tempo # retorna calculo juros simples
+   
+juros_simples(1000,5,6)
+print('TUDO OK')
